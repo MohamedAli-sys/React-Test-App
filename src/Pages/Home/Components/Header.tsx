57605b967cebbea7b1a7b21541c565a8aaabd378
MohamedAli-sys <good.heart52244@gmail.com>
2022-04-06 18:47:08 +0200

    Header Section

--- src/Pages/Home/Components/Header.tsx
+++ src/Pages/Home/Components/Header.tsx
@@ -1,5 +1,7 @@
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FunctionComponent } from "react";
-import Navbar from "../../../Core/Navbar/Navbar";
+import image from './../../../assests/images/HeaderSection.png'
 import './Header.scss'
 
 interface HeaderProps {
@@ -10,6 +12,23 @@ const Header: FunctionComponent<HeaderProps> = () => {
     return (
         <>
             <header className="header">
+                <div className="left-section">
+                    <img src={image} alt="" />
+                </div>
+                <div className="right-section">
+                    <h1>Get creative with .....</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae cumque optio recusandae</p>
+                    <div className="buttons">
+                        <button>
+                            <span>Read More</span>
+                            <FontAwesomeIcon icon={faArrowRightLong} />
+                        </button>
+                        <button>
+                            <span>Get Innovie</span>
+                            <FontAwesomeIcon icon={faArrowRightLong} />
+                        </button>
+                    </div>
+                </div>
             </header>
         </>
     );
