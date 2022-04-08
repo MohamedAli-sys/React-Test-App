60b27808a61f5cbb1424b14db6106b44815b7ae7
MohamedAli-sys <good.heart52244@gmail.com>
2022-04-08 03:34:35 +0200

    animate section

--- src/Pages/Home/Components/AboutComp/AboutComp.tsx
+++ src/Pages/Home/Components/AboutComp/AboutComp.tsx
@@ -1,52 +1,55 @@
 import { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
 import bgimg1 from '../../../../assests/images/h2-icon-backround-img-1.png';
 import bgimg2 from '../../../../assests/images/h2-icon-backround-img-2.png';
 import bgimg3 from '../../../../assests/images/h2-icon-backround-img-3.png';
 import img1 from '../../../../assests/images/h2-icon-img-1.png';
 import img2 from '../../../../assests/images/h2-icon-img-2.png';
 import img3 from '../../../../assests/images/h2-icon-img-3.png';
-import './AboutComp.scss'
+import './AboutComp.scss';
+
 interface AboutCompProps {
 
 }
 
 const AboutComp: FunctionComponent<AboutCompProps> = () => {
+
     return (
         <section className="about-comp">
             <div className="cards">
                 <div className="card">
-                    <a href="" className="content">
+                    <Link to={'about'} className="content">
                         <img src={img1} />
                         <img src={bgimg1} />
-                    </a>
+                    </Link>
                     <div className="card-body">
-                        <a href="" className="card-header">
+                        <Link to={'about'} className="card-header">
                             <h5>Design Proposals</h5>
-                        </a>
+                        </Link>
                         <p>Lorem proin gravida nibh vel velit auliquean nisi elit sollicitudin lora auctor, nisi elit.</p>
                     </div>
                 </div>
                 <div className="card">
-                    <a href="" className="content">
+                    <Link to={'services'} className="content">
                         <img src={img2} />
                         <img src={bgimg2} />
-                    </a>
+                    </Link>
                     <div className="card-body">
-                        <a href="" className="card-header">
-                            <h5>Design Proposals</h5>
-                        </a>
+                        <Link to={'services'} className="card-header">
+                            <h5>Going into action</h5>
+                        </Link>
                         <p>Lorem proin gravida nibh vel velit auliquean nisi elit sollicitudin lora auctor, nisi elit.</p>
                     </div>
                 </div>
                 <div className="card">
-                    <a href="" className="content">
+                    <Link to={'about'} className="content">
                         <img src={img3} />
                         <img src={bgimg3} />
-                    </a>
+                    </Link>
                     <div className="card-body">
-                        <a href="" className="card-header">
-                            <h5>Design Proposals</h5>
-                        </a>
+                        <Link to={'about'} className="card-header">
+                            <h5>Project planning</h5>
+                        </Link>
                         <p>Lorem proin gravida nibh vel velit auliquean nisi elit sollicitudin lora auctor, nisi elit.</p>
                     </div>
                 </div>
