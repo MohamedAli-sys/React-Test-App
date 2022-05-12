c1a4774970137fc2557a30f7b48ef1e2f37c8dd7
MohamedAli-sys <good.heart52244@gmail.com>
2022-05-12 08:36:33 +0200

    Delete User

--- src/Shared/UserCard/UserCard.tsx
+++ src/Shared/UserCard/UserCard.tsx
@@ -1,12 +1,27 @@
 import { FunctionComponent } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import IUsers from "../../interfaces/users";
+import { axiosInst } from "../../Network";
 import './UserCard.scss'
 interface UserCardProps {
     user: IUsers
 }
 
 const UserCard: FunctionComponent<UserCardProps> = ({ user }) => {
+    const deleteUser = (id: number) => {
+        axiosInst.delete(`users/${id}`).then((res) => {
+            toast.success('User Deleted', {
+                position: 'top-right',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined
+            })
+        })
+    }
     return (
         <>
             <div className="user-card">
@@ -17,6 +32,7 @@ const UserCard: FunctionComponent<UserCardProps> = ({ user }) => {
                     Email: {user.email}
                 </h6>
                 <Link className="button__arrows" to={`/ourTeam/Edit/${user.id}`} ><span>Edit</span></Link>
+                <button className="button__arrows" onClick={() => deleteUser(user.id!)}><span>Delete</span></button>
             </div>
         </>
     );
