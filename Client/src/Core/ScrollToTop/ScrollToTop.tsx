1f9abbf6676b63ce1f5e15d742f0649e4b003790
MohamedAli-sys <mohamedfarghal62@outlook.com>
2022-05-16 09:41:31 +0200

    Fix Bugs

--- Client/src/Core/ScrollToTop/ScrollToTop.tsx
+++ Client/src/Core/ScrollToTop/ScrollToTop.tsx
@@ -1,35 +1,33 @@
-import { FunctionComponent, useEffect, useState } from "react";
-import './ScrollToTop.scss'
-interface ScrollToTopProps {
-
-}
+import { FunctionComponent, useEffect, useState } from 'react';
+import './ScrollToTop.scss';
+interface ScrollToTopProps {}
 
 const ScrollToTop: FunctionComponent<ScrollToTopProps> = () => {
-    const [appear, setAppear] = useState(false)
+    const [appear, setAppear] = useState(false);
     const windowAction = () => {
         if (window.scrollY > 400) {
-            setAppear(true)
+            setAppear(true);
         } else {
-            setAppear(false)
+            setAppear(false);
         }
-    }
+    };
     useEffect(() => {
-        window.addEventListener('scroll', windowAction)
+        window.addEventListener('scroll', windowAction);
         return () => {
-            window.removeEventListener('scroll', windowAction)
-        }
-    }, [])
+            window.removeEventListener('scroll', windowAction);
+        };
+    }, []);
     const toTop = () => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
-        })
-    }
+        });
+    };
     return (
         <>
-            <a onClick={toTop} className={`back-to-top ${appear && 'toTopAppear'}`}></a>
+            <button onClick={toTop} className={`back-to-top ${appear && 'toTopAppear'}`}></button>
         </>
     );
-}
+};
 
 export default ScrollToTop;
